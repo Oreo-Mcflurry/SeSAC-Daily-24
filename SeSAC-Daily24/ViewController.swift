fd388383675f6474e9379c83b8b18bd918a5fd45
유인호 <yuiop1029@naver.com>
2024-01-27 15:34:17 +0900

    Initial Commit

---
+++ SeSAC-Daily24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-Daily24
+//
+//  Created by A_Mcflurry on 1/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
